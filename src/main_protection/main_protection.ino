b2e921b52b0ffc4abbcd48e91a2903716f10a339
Fandi Adinata <09.fandisurya@gmail.com>
2021-03-05 18:43:30 +0700

    add voltage protection

--- src/main_protection/main_protection.ino
+++ src/main_protection/main_protection.ino
@@ -1,28 +1,50 @@
 /*
- * Protection circuit for 48V input to inverter 
- * 
- * Last update : 5 Maret 2021
- * 
- */
+   Protection circuit for 48V input to inverter
 
- 
+   Last update : 5 Maret 2021
+
+*/
+
+
 #define VBAT_PIN A0
 #define CURRENT_PIN A1
 #define VPROCTEC_PIN 8
 #define CPROTEC_PIN 7
 
+#define R1 56000
+#define R2 5100
 
-void setup(){
+//Voltage from voltage divider battery (A0)
+int input_battery;
+double battery_voltage;
+
+void setup() {
   Serial.begin(115200);
   pinMode(VBAT_PIN, INPUT);
   pinMode(CURRENT_PIN, INPUT);
   pinMode(VPROTEC_PIN, OUTPUT);
   pinMode(CPROTEC_PIN, OUTPUT);
-
-  
 }
 
-void loop(){
-  //Kosong
+void loop() {
+  //Read battery value from voltage divider (adc)
+  input_battery = analogRead(A0);
+  battery_voltage = ((input_battery / 1023) * 5) * ((R2 + R1) / R1);
+
+  if (battery_voltage > 50 || battery_voltage < 35) {
+    digitalWrite(VPROTEC_PIN, LOW);
+  }
+  if (battery_voltage <= 50 && battery_voltage >= 35) {
+    digitalWrite(VPROTEC_PIN, HIGH);
+  }
+
+  print_data();
+}
+
+
+void print_data() {
+  Serial.print("Tegangan baterai : ");
+  Serial.println(battery_voltage);
+
 }
 
